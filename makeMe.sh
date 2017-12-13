e12c0567575a03045fd9c8824d90ffd17d50b8f5
TimDzik <timodzik@gmail.com>
2017-12-14 00:31:45 +0100

    Make me hmmmm

--- makeMe.sh
+++ makeMe.sh
@@ -20,7 +20,8 @@ else
   sudo apt-get update
   sudo apt-get install git
   sudo apt-get install nodejs
-  sudo apt-get install npm
+  curl -sL https://deb.nodesource.com/setup_6.x -o nodesource_setup.sh
+  sudo bash nodesource_setup.sh
 fi
 
 
